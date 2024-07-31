9f00b2d910627a803d4ad2fe5bc7ae172e1af621
Tomas Garrido <garridot210@gmail.com>
2024-07-31 16:38:19 -0300

    Implement rate limiting, API key authentication, and logging for email API as security measures.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from flask_mail import Mail, Message
-import re
-
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from markupsafe import escape
 from dotenv import load_dotenv
+from functools import wraps
+import logging
+from logging.handlers import RotatingFileHandler
+import re
 import os
 
-load_dotenv() # take environment variables from .env.
+# Load environment variables
+load_dotenv()
 
+# Flask application configuration
 app = Flask(__name__)
 
 app.config['MAIL_SERVER']   = os.getenv('MAIL_SERVER')
@@ -18,38 +25,75 @@ app.config['MAIL_USE_SSL']  = os.getenv('MAIL_USE_SSL').lower() in ['true', '1',
 
 mail = Mail(app)
 
+# Security and rate limiting configuration
+PERSONAL_EMAIL = os.getenv('PERSONAL_EMAIL')
+API_KEY = os.getenv('API_KEY')
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"]
+)
+
+# Logging configuration
+handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+
+# Helper functions
 def is_valid_email(email):
+    """Validates if an email address is valid."""
     regex = r'^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     return re.match(regex, email)
 
+def require_api_key(f):
+    """Decorator to require an API key."""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        key = request.headers.get('x-api-key')
+        if key and key == API_KEY:
+            return f(*args, **kwargs)
+        else:
+            return jsonify({'error': 'Unauthorized access'}), 401
+    return decorated_function
+
+# Application routes
 @app.route('/send_email_api', methods=['POST'])
+@limiter.limit("5 per minute") 
+@require_api_key
 def send_email_api():
-    data    = request.get_json()  
-    email   = data.get('email')
+    """Handle the route to send emails."""
+
+    app.logger.info('Request received from %s', request.remote_addr)
+
+    data = request.get_json()  
+    email = data.get('email')
     message = data.get('message')
 
     if not email or not message:
         return jsonify({'error': 'All fields are required.'}), 400  
-
     if not is_valid_email(email):
         return jsonify({'error': 'The email address is invalid.'}), 400      
-
     if len(message.strip()) == 0:   
-        return jsonify({'error': 'The message cannot be empty.'}), 400  
+        return jsonify({'error': 'The message cannot be empty.'}), 400 
+
+    sanitized_message = escape(message)  
 
     try:
         msg = Message(
-            f'Email from {email}',
-            sender="darden246@gmail.com",
-            recipients=["garridot210@gmail.com"]
+            f'Email from {email} via Email Center API',
+            sender=os.getenv('MAIL_USERNAME'),
+            recipients=[PERSONAL_EMAIL]
         )
-        msg.body = message
+        msg.body = sanitized_message
         mail.send(msg)
         return jsonify({'success': 'The email was sent successfully.'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500  
-              
 
+# Main to run the application
 if __name__ == '__main__':
     app.run(debug=True)
+
 
